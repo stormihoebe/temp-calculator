342e4cc74ed482b2a18286669d392f43e940ec19
Derek DeMaria <demariaderek@gmail.com>
2017-08-01 14:46:15 -0700

    Remove App to simplify Component tree

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import Calculator from './Components/Calculator';
-import './App.css';
-
-/** App Component Tree
-  App
-  |- Calculator
-      |- BoilingVerdict
-**/
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
-
-export default App;
+// import React, { Component } from 'react';
+// import Calculator from './Components/Calculator';
+// import './App.css';
+//
+// /** App Component Tree
+//   App
+//   |- Calculator
+//       |- BoilingVerdict
+// **/
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Calculator />
+//       </div>
+//     );
+//   }
+// }
+// 
+// export default App;
